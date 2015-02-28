4bbcd04016150655c41763ab0b04de0840477812
Rob Fielding <rob.fielding@gmail.com>
2015-02-27 23:35:15 -0500

    make it look like an object, and stop allowing non-sparse arraya

--- gaMul.py
+++ gaMul.py
@@ -22,42 +22,44 @@ class SparseArray:
             kvs.append(("{0:0%db}:{1}" % dims).format(k,v))
         return "{ %s }" % (", ".join(kvs))
 
-#Count the number of swaps to sort indices and get right sign, and xor indices to get return index
-def destIdx(ai,bi):
-    retidx, multiplier = ai^bi, 1
-    stack, didSwap = [], True
-    for i in [ai,bi]: #within an index, we order from right to left!!
-        iidx = 0
-        while i != 0:
-            if i & 0b01:
-                stack.append(iidx)
-            i, iidx = i >> 1, iidx + 1
-    while didSwap:
-        didSwap = False
-        for s in range(0,len(stack)-1):
-            if stack[s] > stack[s+1]:
-                stack[s], stack[s+1] = stack[s+1], stack[s]
-                multiplier, didSwap = multiplier * -1, True
-    return retidx, multiplier
+    def __iter__(self):
+        for k in self.data:
+            yield k
 
-#Multiplication is iterating over O( 2^(2d) ) possible contributions to place them in the right part of the result
-def mul(a,b):
-    d =  dims
-    answer = SparseArray()
-    n = (1<<d) #only required to figure out how far to compute. Sparse arrays may tell us this too
-    for ai in range(0,n):
-        for bi in range(0,n):
-            i, multiplier = destIdx(ai,bi)
-            answer[i] += multiplier * a[ai] * b[bi]
-    return answer
+    def destIdx(self, ai,bi):
+        retidx, multiplier = ai^bi, 1
+        stack, didSwap = [], True
+        for i in [ai,bi]: #within an index, we order from right to left!!
+            iidx = 0
+            while i != 0:
+                if i & 0b01:
+                    stack.append(iidx)
+                i, iidx = i >> 1, iidx + 1
+        while didSwap:
+            didSwap = False
+            for s in range(0,len(stack)-1):
+                if stack[s] > stack[s+1]:
+                    stack[s], stack[s+1] = stack[s+1], stack[s]
+                    multiplier, didSwap = multiplier * -1, True
+        return retidx, multiplier
 
-def add(a,b):
-    d = dims
-    answer = SparseArray()
-    n = (1<<d)
-    for ai in range(0,n):
-        answer[ai] += a[ai] + b[ai]
-    return answer
+    def mul(self,b):
+        a = self
+        d =  dims
+        answer = SparseArray()
+        for ai in a:
+            for bi in b:
+                i, multiplier = self.destIdx(ai,bi)
+                answer[i] += multiplier * a[ai] * b[bi]
+        return answer
+
+    def add(self,b):
+        a = self
+        d = dims
+        answer = SparseArray()
+        for ai in a:
+            answer[ai] += a[ai] + b[ai]
+        return answer
 
 a=SparseArray()
 a1=SparseArray()
@@ -70,5 +72,5 @@ a1[0b100] = 3
 #Checks out with example from: http://math.stackexchange.com/questions/916998/product-between-multivectors
 print str(a)
 print str(a1)
-print str(mul(a, a1))
+print str(a.mul(a1))
 
